6735dc39f6126ff6546b4e697e2ea6202287a154
Alex <ascheglov17@gmail.com>
2020-08-04 02:26:12 +0200

    Intro section finished(mobile devices)

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,17 @@
 let burger = document.querySelector('#burger'),
-    navMenu = document.querySelector('#nav-menu');
-
+    burgerElement = document.querySelector('#burger_element'),
+    navMenu = document.querySelector('#nav-menu'),
+    opened = 'burger__item--opened',
+    closed = 'burger__item--closed';
 burger.addEventListener('click', function(){
     if(navMenu.style.right=='-320px'){
         navMenu.style.right = '0px';
+        burgerElement.classList.remove(closed);
+        burgerElement.classList.add(opened);
+        
     } else{
         navMenu.style.right = '-320px';
+        burgerElement.classList.remove(opened);
+        burgerElement.classList.add(closed);
     }
 });
