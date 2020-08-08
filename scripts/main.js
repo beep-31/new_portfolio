bb3512d9228f992278bce2b31abeade236c59f7b
Alex <ascheglov17@gmail.com>
2020-08-08 16:42:03 +0200

    Mobile version finished

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,34 @@ burger.addEventListener('click', function(){
         burgerElement.classList.add(closed);
     }
 });
+
+let mail_input = document.querySelector('#input__email'),
+    mail_label = document.querySelector('#email__label');
+
+
+mail_input.addEventListener('focusout', function(){
+    if(!mail_input.checkValidity()){
+        mail_input.style.borderColor = '#fc1f17';
+        mail_label.style.color = '#fc1f17';
+        mail_label.style.top = '-10px';
+        mail_label.style.fontSize = '10px';
+    } else if(mail_input.checkValidity()){
+        mail_input.style.borderColor = '#00ffff';
+        mail_label.style.color = '#cecece';
+        mail_label.style.top = '-10px';
+        mail_label.style.fontSize = '10px';
+    }
+    if (mail_input.value == ''){
+        mail_input.style.borderColor = '#00ffff';
+        mail_label.style.color = '#cecece';
+        mail_label.style.top = '0px';
+        mail_label.style.fontSize = '16px';
+    }
+});
+
+mail_input.addEventListener('focus', function(){
+    mail_input.style.borderColor = '#fc5417';
+    mail_label.style.color = '#cecece';
+    mail_label.style.top = '-10px';
+    mail_label.style.fontSize = '10px';
+});
