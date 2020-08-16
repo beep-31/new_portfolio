3f7c768038ae4f4e6e7cd8c3530b6f13988e5008
Alex <ascheglov17@gmail.com>
2020-08-16 16:05:26 +0200

    Some tablet and desktop design were applied

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// burger menu 
 let burger = document.querySelector('#burger'),
     burgerElement = document.querySelector('#burger_element'),
     navMenu = document.querySelector('#nav-menu'),
@@ -16,9 +17,43 @@ burger.addEventListener('click', function(){
     }
 });
 
+//slider
+
+function sliderHide(content, btns){
+    for(let i = 0; i<content.length;i++){
+        content[i].classList.remove('about__txt--active');
+    }
+
+    for(let i = 0; i<btns.length;i++){
+        btns[i].classList.remove('lang--active');
+    }
+}
+
+function sliderShow(n){
+    sliderTxt[n].classList.add('about__txt--active');
+    sliderBtns[n].classList.add('lang--active');
+}
+
+let sliderTxt = document.querySelectorAll('.about__txt'),
+    sliderBtns = document.querySelectorAll('.lang'),
+    slider = document.querySelector('#slider');
+
+slider.addEventListener('click', function(event){
+    let target = event.target;
+    if(target && target.classList.contains('lang')){
+        sliderHide(sliderTxt, sliderBtns);
+        for(let i = 0;i<sliderBtns.length;i++){
+            if(target==sliderBtns[i]){
+                sliderShow(i);
+            }
+        }
+    }
+});
+
+//contact form
+
 let mail_input = document.querySelector('#input__email'),
     mail_label = document.querySelector('#email__label');
-
 
 mail_input.addEventListener('focusout', function(){
     if(!mail_input.checkValidity()){
